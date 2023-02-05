9b67236b22548bdf4610be187d8a0dca3cc48739
Wayne-Thornberry <way-net@outlook.com>
2023-02-05 18:33:29 +0000

    :)

--- deploy.ps1
+++
@@ -1,7 +0,0 @@
-﻿Remove-Item "D:\ProjectOnline\resources\client-core\*"  -Recurse
-Copy-Item -Path ".\data\core\*" -Destination "D:\ProjectOnline\resources\client-core\" -Recurse 
-Copy-Item -Path ".\data\components\ProlineCore\*" -Destination "D:\ProjectOnline\resources\client-core\" -Recurse
-Copy-Item -Path ".\vendor\ProlineCore\*" -Destination "D:\ProjectOnline\resources\client-core\" -Recurse
-Copy-Item -Path ".\artifacts\*" -Destination "D:\ProjectOnline\resources\client-core\" -Recurse
-Remove-Item "D:\ProjectOnline\resources\client-core\CitizenFX.Core.*.dll" -Recurse
-Remove-Item "D:\ProjectOnline\resources\client-core\*.pdb" -Recurse
